e07bc51d4f18837e27eedf3a4699792d65ff9a53
garchy-home <jandro.garcia.sanchez@gmail.com>
2024-12-12 17:35:39 +0100

    12-12-24

--- DisIf/VSCODE/Angular/my-project/src/app/ej01/ej01.component.ts
+++ DisIf/VSCODE/Angular/my-project/src/app/ej01/ej01.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ej01.component.css']
 })
 export class Ej01Component implements OnInit {
+  numbers : Array<Number> = [1,2,3,4,5];
+  varColor : boolean = true;
+
+  customers = [{value: 'Ebony'}, {value: 'Chiho'}];
+  customer = 'Padma';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeColor() {
+    this.varColor = !this.varColor;
+    
+  }
+
+  onKeydown(event: any){
+    // console.log(event);
+    console.log((event as KeyboardEvent).key);
+    if((event as KeyboardEvent).key == "Delete"){
+      console.clear();
+    }
+  }
+
 }
+
